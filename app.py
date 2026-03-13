7b9f35e16c06f56b1b93e92de16749fd8bd2d3d6
Shamroz Abrar <shamrozahmad2412@gmail.com>
2026-03-13 10:07:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-st.set_page_config(page_title="HPV EPIPRED", page_icon="🧬", layout="wide")
+st.set_page_config(page_title="HPV EPIPRED", page_icon="🧬", layout="centered")
 
 st.markdown("""
 <style>
