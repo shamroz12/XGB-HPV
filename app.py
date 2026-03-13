37e8bf2fdf9415076c3522a219ea6150300b4746
Shamroz Abrar <shamrozahmad2412@gmail.com>
2026-03-13 10:21:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,6 @@
 import streamlit as st
 st.set_page_config(page_title="HPV EPIPRED", page_icon="🧬", layout="wide")
 
-if "theme" not in st.session_state:
-    st.session_state.theme = "dark"
-
-col1, col2 = st.columns([8,1])
-
-with col2:
-    if st.button("🌙" if st.session_state.theme == "light" else "☀️"):
-        st.session_state.theme = "dark" if st.session_state.theme == "light" else "light"
-        st.rerun()
-
 st.markdown("""
 <style>
 
